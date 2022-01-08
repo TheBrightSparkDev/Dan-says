c722c38470c05ae2c426fa1be7c65d5a34ede0d8
TheBrightSparkDev <danharriesxd@gmail.com>
2022-01-08 13:48:20 +0000

    feat - added the middle button and created tests to make sure all elements are clickable

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,3 +1,6 @@
 console.log("connected");
 document.getElementById("red").addEventListener("click", function(){console.log("red clicked");});
 document.getElementById("yellow").addEventListener("click", function(){console.log("yellow clicked");});
+document.getElementById("blue").addEventListener("click", function(){console.log("blue clicked");});
+document.getElementById("green").addEventListener("click", function(){console.log("green clicked");});
+document.getElementById("middle").addEventListener("click", function(){console.log("middle clicked");});
