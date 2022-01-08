8ec5c46ce8adb4ab9a7f88cc9a4da37a1727c41c
TheBrightSparkDev <danharriesxd@gmail.com>
2022-01-08 04:10:41 +0000

    feat - added js files and tests to see if they are correctly connected and picking up the elements they need to

---
+++ assets/js/gameLogic.js
@@ -0,0 +1,3 @@
+console.log("connected");
+document.getElementById("red").addEventListener("click", function(){console.log("red clicked");});
+document.getElementById("yellow").addEventListener("click", function(){console.log("yellow clicked");});
