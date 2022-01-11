3cae9016c9ed6eded83ae7e068f0513fd0aa3d01
TheBrightSparkDev <danharriesxd@gmail.com>
2022-01-11 00:14:55 +0000

    feat - added the first working function to gameLogic.js and a few broken ones

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,9 +1,32 @@
-console.log("connected");
+function checkAnswer(){
+
+};
+function timer(timeLeft){
+    TimeUnit.seconds.sleep(1);
+    if(timeLeft === 0){
+        checkAnswer()
+        console.log("checkAnswer called")
+    } else {
+        timeLeft -= 1;
+        console.log("timeLeft = " + timeLeft)
+        timer(timeLeft);
+        return $("#timer").html`
+        ${timeLeft}`;
+    }
+    
+};
+function playGame(){
+    timer(5);
+    return $("#middle").html`
+    <h1 id="timer">5</h1>`;
+};
+console.log("just checking");
+document.getElementById("middle").addEventListener("click", function(){playGame()});
+console.log("checking again");
 document.getElementById("red").addEventListener("click", function(){console.log("red clicked");});
 document.getElementById("yellow").addEventListener("click", function(){console.log("yellow clicked");});
 document.getElementById("blue").addEventListener("click", function(){console.log("blue clicked");});
 document.getElementById("green").addEventListener("click", function(){console.log("green clicked");});
-document.getElementById("middle").addEventListener("click", function(){console.log("middle clicked");});
 document.getElementById("main-menu-top-left").addEventListener("click", function(){console.log("theme clicked");});
 document.getElementById("main-menu-top-right").addEventListener("click", function(){console.log("suggestions clicked");});
 document.getElementById("main-menu-bottom-left").addEventListener("click", function(){console.log("tutorial clicked");});
