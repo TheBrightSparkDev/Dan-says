ee20d06d061ebbab6a9d8671474982b66b3f11f1
TheBrightSparkDev <danharriesxd@gmail.com>
2022-01-11 22:17:36 +0000

    feat-created the timer function and tested to make sure it works

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,21 +1,38 @@
-function checkAnswer(){
+function correct(){};
+function gameOver(){};
+function answer(){};
+function command(){
 
 };
-function timer(timeLeft){
-    TimeUnit.seconds.sleep(1);
-    if(timeLeft === 0){
-        checkAnswer()
-        console.log("checkAnswer called")
+function checkAnswer(choice){
+    console.log("checkAnswer called");
+    let answer = answer();
+    if (choice === answer){
+        correct();
     } else {
-        timeLeft -= 1;
-        console.log("timeLeft = " + timeLeft)
-        timer(timeLeft);
-        return $("#timer").html`
-        ${timeLeft}`;
+        gameOver();
     }
-    
 };
+function updateTimer(time){
+    console.log("timeLeft = " + time);
+        $("#middle").html(`
+        <h1 id="timer">${time}</h1>`);
+        time--;
+        /* https://dev.to/gspteck/create-a-stopwatch-in-javascript-2mak this site was a massive help here */
+        setTimeout(function(){timer(time)},1000);
+};
+
+function timer(timeLeft){
+    if (timeLeft===0){
+        $("#middle").html(`
+        <h1 id="timer">${timeLeft}</h1>`);
+        checkAnswer(middle);
+    } else {
+        updateTimer(timeLeft);
+    }
+}
 function playGame(){
+    command();
     timer(5);
     return $("#middle").html`
     <h1 id="timer">5</h1>`;
