3544b9965d07ec56fb08187dddb3da19603a06f7
TheBrightSparkDev <danharriesxd@gmail.com>
2022-01-12 00:11:37 +0000

    fix - removed bug where all buttons where being called at the beginning and causing a gameOver Event to be called

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,9 +1,17 @@
-function correct(){};
-function gameOver(){};
+/* declaring global variables */
+var color = "";
+var dont = "";
+var name = "";
+/* what happens when correct answer is given */
+function correct(){
+    console.log("correct");
+};
+/* what happens when wrong answer is given */
+function gameOver(){
+    console.log("gameOver");
+};
+/* Tells user what to do */
 function command(){
-    var color = "";
-    var dont = "";
-    var name = "";
     let colorPicker = Math.floor((Math.random() * 100) + 1);
     if (colorPicker <= 25){
         color = "red";
@@ -30,10 +38,29 @@ function command(){
     $("#main-menu").html(`
     <h3 id="command">${name} says ${dont} press ${color}</h3>`)
 };
+/* checks if user gave correct answer */
 function checkAnswer(choice){
     console.log("checkAnswer called");
     console.log(choice);
+    if (name === "Simon"){
+        if (choice === "middle"){
+            correct();
+        } else {
+            gameOver();
+        }        
+    } else if (dont === "dont"){
+        if (choice === "middle"){
+        correct();
+        } else {
+            gameOver();
+        }
+    } else if (color === choice){
+        correct();
+    } else {
+        gameOver();
+    }
 };
+/* timer functions */
 function updateTimer(time){
     console.log("timeLeft = " + time);
         $("#middle").html(`
@@ -42,7 +69,6 @@ function updateTimer(time){
         /* https://dev.to/gspteck/create-a-stopwatch-in-javascript-2mak this site was a massive help here */
         setTimeout(function(){timer(time)},1000);
 };
-
 function timer(timeLeft){
     if (timeLeft===0){
         $("#middle").html(`
@@ -52,19 +78,37 @@ function timer(timeLeft){
         updateTimer(timeLeft);
     }
 }
+/* game button presses */
+function redPressed(){
+    checkAnswer("red");
+};
+function yellowPressed(){
+    checkAnswer("yellow");
+};
+function bluePressed(){
+    checkAnswer("blue");
+};
+function greenPressed(){
+    checkAnswer("green");
+};
+function middlePressed(){
+    checkAnswer("middle");
+};
+/* starts the game */
 function playGame(){
     command();
     timer(5);
     document.getElementById("middle").removeEventListener("click",playGame);
-    document.getElementById("middle").addEventListener("click", checkAnswer(middle));
+    document.getElementById("middle").addEventListener("click", middlePressed);
     return $("#middle").html`
     <h1 id="timer">5</h1>`;
 };
+/* Event Listeners */
 document.getElementById("middle").addEventListener("click", playGame);
-document.getElementById("red").addEventListener("click", checkAnswer("red"));
-document.getElementById("yellow").addEventListener("click", checkAnswer("yellow"));
-document.getElementById("blue").addEventListener("click", checkAnswer("blue"));
-document.getElementById("green").addEventListener("click", checkAnswer("green"));
+document.getElementById("red").addEventListener("click", redPressed);
+document.getElementById("yellow").addEventListener("click", yellowPressed);
+document.getElementById("blue").addEventListener("click", bluePressed);
+document.getElementById("green").addEventListener("click", greenPressed);
 document.getElementById("main-menu-top-left").addEventListener("click", function(){console.log("theme clicked");});
 document.getElementById("main-menu-top-right").addEventListener("click", function(){console.log("suggestions clicked");});
 document.getElementById("main-menu-bottom-left").addEventListener("click", function(){console.log("tutorial clicked");});
